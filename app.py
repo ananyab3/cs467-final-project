e97a0ef8ba8e2e225e4883f2c356e5bbdfadb8fa
Ananya Barman <ananyab3@illinois.edu>
2024-04-29 15:03:47 -0500

    making map

--- app.py
+++ app.py
@@ -1,16 +1,45 @@
 from shiny import App, render, ui
+import pandas as pd
+import plotly.express as px
+import plotly.graph_objects as go 
+from shinywidgets import output_widget, render_widget
+from datetime import datetime
+
+from ipywidgets import widgets, interactive, Output
+
+df1 = pd.read_csv("data/all_apartments.csv")
+df2 = pd.read_csv('data/all_nuisance_complaints.csv')
 
 app_ui = ui.page_fluid(
-    ui.panel_title("Hello Shiny!"),
-    ui.input_slider("n", "N", 0, 100, 20),
-    ui.output_text_verbatim("txt"),
+    ui.panel_title("Nuisance Complaints & Apartments in Urbana"),
+    ui.navset_pill(  
+    ui.nav_panel("Map", output_widget("mapped", width="100%", height="1000px")),
+    id="tab",  
+)
 )
 
 
 def server(input, output, session):
-    @render.text
-    def txt():
-        return f"n*2 is {input.n() * 2}"
+    @render_widget
+    def mapped():
+        fig = px.scatter_mapbox(df1, lat='Latitude', lon='Longitude',
+                            hover_name='Address', hover_data=['Price per Bed', '# Beds'],
+                            zoom=10, color_discrete_sequence=['blue'], opacity=0.7)
+    
+        # Add trace for the second dataset
+        fig.add_scattermapbox(
+            lat=df2['Latitude'], lon=df2['Longitude'],
+            mode='markers',
+            hoverinfo='text',
+            hovertext=df2['Type of Complaint'],
+            marker=dict(size=3, color='red', opacity=0.7),
+            name='Complaints'
+        )
 
+        # Update layout
+        fig.update_layout(mapbox_style="open-street-map")
+        fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0}, height=800)
+        
+        return fig
 
 app = App(app_ui, server)

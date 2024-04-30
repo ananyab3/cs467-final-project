8a0c4ed4e5a42f1becf95ce00937c073ace186f6
Ananya Barman <ananyab3@illinois.edu>
2024-04-29 21:28:20 -0500

    added new visualizations

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ import plotly.graph_objects as go
 from shinywidgets import output_widget, render_widget
 from datetime import datetime
 from ipywidgets import widgets, interactive, Output
+import seaborn as sns
 
 df1 = pd.read_csv("data/all_apartments.csv")
 df1['Price per Bed'] = df1['Price per Bed'].astype('int')
@@ -15,7 +16,9 @@ df2 = pd.read_csv('data/all_nuisance_complaints.csv')
 app_ui = ui.page_fluid(
     ui.panel_title("Nuisance Complaints & Apartments in Urbana"),
     ui.navset_pill(
-        ui.nav_panel("Map",
+        ui.nav_panel("# of Beds vs. Price per Bed", output_widget("bar_chart")),
+        ui.nav_panel("Disposition Categories for Nuisances", output_widget("pie_chart")),
+        ui.nav_panel("Map of Apartments and Nuisance Complaints",
                     ui.input_slider("price_per_bed", "Price per Bed", 
                         min=df1['Price per Bed'].min(), max=df1['Price per Bed'].max(),
                         value=[df1['Price per Bed'].min(), df1['Price per Bed'].max()]),
@@ -65,6 +68,39 @@ def server(input, output, session):
         fig.update_layout(mapbox_style="open-street-map")
         fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0}, height=500)
         
+        return fig
+    @render_widget
+    def bar_chart():
+        fig = go.Figure(data=go.Scatter(x=df1['# Beds'],
+                                y=df1['Price per Bed'],
+                                mode='markers',
+                                marker_color='green'))
+
+        fig.update_layout(
+                        xaxis_title='# of Beds',
+                        yaxis_title='Price per Bed ($)',
+                        autosize=False,
+                        width=1000,
+                        height=600,
+                        margin=dict(l=50, r=50, b=100, t=100, pad=4),
+                        paper_bgcolor="LightSteelBlue",)
+        return fig
+    @render_widget
+    def pie_chart():
+        disposition_counts = df2['Disposition'].value_counts()
+        colorblind_palette = sns.color_palette("colorblind", len(disposition_counts))
+
+        fig = go.Figure(data=go.Pie(labels=disposition_counts.index,
+                                    values=disposition_counts,
+                                    textinfo='label+percent',
+                                    insidetextorientation='radial',
+                                    marker=dict(colors=colorblind_palette.as_hex())))
+
+        fig.update_layout(autosize=False,
+                        width=1000,
+                        height=600,
+                        margin=dict(l=50, r=50, b=100, t=100, pad=4),
+                        paper_bgcolor="LightSteelBlue",)
         return fig
 
 app = App(app_ui, server)
